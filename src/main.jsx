e5bb5000034d167ff092c89958c4fde20e9f4acf
Prathamesh Sawle <sawleprathamesh@gmail.com>
2024-09-29 18:52:10 -0400

    Set up Redux Provider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+// Import necessary modules
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// Import Provider from react-redux to provide the Redux store to the app
+import { Provider } from 'react-redux';
+
+// Import the Redux store from the store.js file
+import store from './store.js';
+
+// Wrap the App with Provider and pass the store so that Redux store is available globally
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
